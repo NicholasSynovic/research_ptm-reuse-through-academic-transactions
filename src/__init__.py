df6b47c4853a7dc0b20766c744f91c9e87c7e6e2
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-05-23 14:22:22 -0500

    Add global variables

---
+++ src/__init__.py
@@ -0,0 +1,6 @@
+from typing import List
+
+
+APP_TITLE: str = "PTM Reuse Through Academic Transactions"
+APP_Description: str = "Leveraging academic works to identify the provenance of Pre-Trained Deep Neural Networks"
+APP_AUTHORS: List[str] = ["Francis Olakangil", "Nicholas M. Synovic", "George K. Thiruvathukal",]
