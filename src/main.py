4469fc0ef3d8fe2bdd89da7c337f0700d258efcd
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-05-23 15:07:29 -0500

    Start refactoring code

--- src/main.py
+++ src/main.py
@@ -4,6 +4,47 @@ from pathlib import Path
 import pandas as pd
 import streamlit as st
 from sqlalchemy import Engine, create_engine
+from streamlit.runtime.uploaded_file_manager import UploadedFile
+
+from src import APP_AUTHORS, APP_DESCRIPTION, APP_TITLE
+
+
+def debug_PrintFileStats(file: UploadedFile) -> None:
+    st.write(file.file_id)
+    st.write(file.name)
+    st.write(file)
+
+
+def buildPage() -> None:
+    st.title(body=APP_TITLE)
+    st.markdown(
+        body=f"> {APP_DESCRIPTION}",
+        help=f"Created by {', '.join(APP_AUTHORS)}",
+    )
+    st.divider()
+
+    st.markdown(body="## SQLite Database Picker")
+    st.markdown(body="> Pick a SQLite3 database to utilize")
+    dbFile: UploadedFile | None = st.file_uploader(
+        label="SQLite3 Database Uploader",
+        type="db",
+        accept_multiple_files=False,
+        help="No database will be created or copied, however, the filename of \
+                the database will be used to establish a connection",
+        on_change=debug_PrintFileStats,
+    )
+
+    st.markdown(body="## DOI Search")
+    st.markdown(body="> Search for DOIs captured within our database")
+    with st.form(key="doi-search", clear_on_submit=False, border=True):
+        doiInput: str | None = st.text_input(
+            "DOI Search Bar",
+            value="10.48550/arXiv.2404.14619",
+            help='Input can be in the form of \
+"https://doi.org/10.48550/arXiv.2404.14619" or \
+"10.48550/arXiv.2404.14619"',
+        )
+        formSubmit: bool = st.form_submit_button(label="Search")
 
 
 def print_doi_df():
@@ -95,7 +136,7 @@ def main() -> None:
 
 
 if __name__ == "__main__":
-    main()
+    buildPage()
 
 
 # streamlit run main.py
