9e8202ffd644eb963de0150a5b135370d94c446a
Francis Olakangil <fj.olakangil@gmail.com>
2024-06-03 14:58:11 -0500

    working on proportion of PM in OA

--- src/stats.py
+++ src/stats.py
@@ -1,25 +1,16 @@
 import sqlite3
 from sqlite3 import Connection, Cursor
+from typing import Iterator
 from urllib.parse import urlparse
 
 import pandas as pd
+from pandas import DataFrame, Series
 
 
 def count_papers_in_db(column: str, table: str, conn: Connection) -> int:
     query = f"SELECT COUNT(DISTINCT {column}) FROM {table}"
     cursor: Cursor = conn.execute(query)
     return cursor.fetchone()[0]
-
-
-# PeaTMOSS database read-in and calculations
-def generate_PM_stats():
-    PM_id_column = "paper_id"
-    query = f"SELECT {PM_id_column} FROM model_to_paper"
-    PM_model_df = pd.read_sql_query(query, con=PMconn)
-
-    # # of unique papers in PM
-    PM_unique_paper_num = PM_model_df[PM_id_column].nunique()
-    print("# of unique papers in PM db: ", PM_unique_paper_num)
 
 
 # total citations across the board
@@ -42,27 +33,40 @@ def count_papers_per_journalPM(conn: Connection):
     print(df["url"].apply(extract_base_url).value_counts())
 
 
-# unique PM papers in OA dataset
-def generate_unique_PMpapers_OAdataset():
-    # OA titles
+def count_PMarxiv_papers_in_OA(PMconn: Connection, OAconn: Connection):
+    paper_count = 0
 
-    OA_works_column = "title"
-    query = f"SELECT {OA_works_column} FROM works"
-    OA_titles_df = pd.read_sql_query(query, con=conn)
+    query = f"SELECT * FROM paper"
+    PM_df: DataFrame = pd.read_sql_query(query, con=PMconn)
+    arxiv_series: Series = PM_df["url"].str.lower()
+    arxiv_formatted: Series = arxiv_series.str.replace(
+        pat="https://arxiv.org/abs/", repl="10.48550/arxiv."
+    )
 
-    # PM titles
+    OA_query = f"SELECT oa_id, doi FROM works"
+    OA_df: Iterator[DataFrame] = pd.read_sql_query(
+        OA_query, con=OAconn, chunksize=10000
+    )
 
-    PM_id_column = "title"
-    query = f"SELECT {PM_id_column} FROM model_to_paper"
-    PM_titles_df = pd.read_sql_query(query, con=PMconn)
+    df: DataFrame
+    for df in OA_df:
+        df_lower: Series = df["doi"].str.lower()
+        paper_count += df["doi"][df_lower.isin(arxiv_formatted)].size
+    return paper_count
 
-    # finding PM papers that are also in OA dataset; filter duplicate titles
-    OA_titles_unique = OA_titles_df.drop_duplicates()
-    PM_titles_unique = PM_titles_df.drop_duplicates()
 
-    paper_intersection = PM_titles_unique[PM_titles_unique.isin(OA_titles_unique)]
-    num_common_titles = paper_intersection.nunique()
-    print("total num of unique PM papers in OA dataset: ", num_common_titles)
+def proportion_PM_papers_in_OA(PMconn: Connection):
+    query = f"SELECT * FROM paper"
+    PM_df: DataFrame = pd.read_sql_query(query, con=PMconn)
+
+    OA_query = f"SELECT oa_id, doi FROM works"
+    OA_df: Iterator[DataFrame] = pd.read_sql_query(
+        OA_query, con=OAconn, chunksize=10000
+    )
+
+    common_unique_values = set(OA_df) & set(PM_df)
+    proportion = len(common_unique_values) / len(OA_df)
+    print("Proportion of PM papers in OA dataset: ", proportion)
 
 
 OA_file_path = "/Users/fran-pellegrino/Desktop/ptm-reuse_academic_transactions/research_ptm-reuse-through-academic-transactions/nature/db/feedStorage/prod.db"
@@ -72,7 +76,10 @@ PM_file_path = "/Users/fran-pellegrino/Desktop/ptm-reuse_academic_transactions/r
 PMconn = sqlite3.Connection(database=PM_file_path)
 
 if __name__ == "__main__":
-    find_unique_PMpapers_in_OA(PMconn)
+    proportion_PM_papers_in_OA(PMconn)
+    quit()
+    print("PM papers in OA: ", count_PMarxiv_papers_in_OA(PMconn, OAconn))
+    count_papers_per_journalPM(PMconn)
     print(
         "PM database count: ",
         count_papers_in_db(column="paper_id", table="model_to_paper", conn=PMconn),
